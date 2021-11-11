93cdf3d0abf5385cf6dc7ff58ecd6d457429e737
Yash Parekh <yparekh@uncc.edu>
2021-11-11 15:49:13 -0500

    Delete Activity11.js

--- itis3135/scripts/Activity11.js
+++
@@ -1,8 +0,0 @@
-<!-- jQuery call to the accordion() method. -->
-$(document).ready(function() {
-    $("#tabs").accordion({
-		event: "click",
-		heightStyle: "content",
-		collapsible: true
-	});
-});
