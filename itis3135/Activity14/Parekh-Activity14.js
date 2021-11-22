ec6d33d3d1adfb5e2ffe5eecb0d3b7bdcc72a74b
Yash Parekh <yparekh@uncc.edu>
2021-11-21 19:30:04 -0500

    Activity 14

---
+++ itis3135/Activity14/Parekh-Activity14.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+    $("li a").each(function(){
+        // the event handler for clicking each element
+        $(this).click(function(){
+            $.ajax({
+                type: "get",
+                url: "json_files/" + this.title + ".json",
+				beforeSend: function() {$("main").html("Loading...");}, 
+				timeout: 10000, 
+				error: function(xhr, status, error) { 
+					alert("Error: " + xhr.status + " - " + error); 
+				}, 
+                dataType: "json",
+                success: function(data) {
+                    $("main").empty();
+                    $.each(data, function(){
+                        $.each(this, function(key, value){
+                            $("main").append($(
+								"<h1>" + value.title + "</h1>"
+                                + "<h2>" + value.month + "</h2>"
+                                + "<h3>" + value.speaker + "</h3>"
+                                + "<img src=" + value.image + " alt=" + this.title + "_picture>"
+                                + "<p>" + value.text + "</p>")
+                            );
+                        })
+                    });
+                }
+            })
+        });
+    });
+}); // end ready
