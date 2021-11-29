32adc0b47d9c509ce5e712c2c76d28e9e18af09c
Yash Parekh <yparekh@uncc.edu>
2021-11-28 21:52:17 -0500

    Update Parekh-Activity16.js

--- itis3135/Activity16/Parekh-Activity16.js
+++ itis3135/Activity16/Parekh-Activity16.js
@@ -1,12 +1,12 @@
 $(document).ready(function(){
-    // URL found at https://www.flickr.com/services/feeds/docs/photos_public/
+	
     var url = "https://api.flickr.com/services/feeds/photos_public.gne?id=82407828@N07&format=json&jsoncallback=?&tags=vectacorpbuilding";
+	
     $.getJSON(url, function(data){
-        var writeHTML = "";
+        var html = "";
         $.each(data.items, function(i, item){
-            writeHTML += "<a href= \"" + item.media.m + "\"data-lightbox= \"buildingImages\" data-title=" 
-            + item.title + "><img src=" + item.media.m + "></a>";
+            html += "<a href= \"" + item.media.m + "\"data-lightbox= \"buildingImages\" data-title=" + item.title + "><img src=" + item.media.m + "></a>";
         });
-    $("#new_building").html(writeHTML);
+		$("#new_building").html(html);
     });
 });
