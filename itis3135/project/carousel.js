a802d6ae3c68c29800c5fab5158b9280c0b0590f
Yash Parekh <yparekh@uncc.edu>
2021-12-03 19:41:52 -0500

    Project

---
+++ itis3135/project/carousel.js
@@ -0,0 +1,17 @@
+$(function(){
+  $(".carousel").ig3js({
+    manifest: [
+      {src:"DSC_0410.JPG", id:"image1"},
+      {src:"DSC_0189.JPG", id:"image2"},
+      {src:"puddle.jpg", id:"image3"},
+      {src:"DSC_0296.JPG", id:"image4"},
+      {src:"TIGERSTARE.jpg", id:"image5"},
+      {src:"DSC_0342.jpg", id:"image6"},
+      {src:"cycle2.jpg", id:"image7"},
+      {src:"leaf.jpg", id:"image8"},
+      {src:"cycle1.jpg", id:"image9"},
+    ],
+	imagePath:'images/',
+    alphaBackground:true
+  });
+});
