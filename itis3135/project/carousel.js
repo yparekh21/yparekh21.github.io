32a4b7c00364305193363ab62618dbc900b6d4b4
Yash Parekh <yparekh@uncc.edu>
2021-12-03 23:13:15 -0500

    project

--- itis3135/project/carousel.js
+++ itis3135/project/carousel.js
@@ -20,8 +20,8 @@ $(document).ready(function() {
 
 	$('.image-popup-no-margins').magnificPopup({
 		type: 'image',
-		closeOnContentClick: true,
-		closeBtnInside: false,
+		closeOnContentClick: false,
+		closeBtnInside: true,
 		fixedContentPos: true,
 		mainClass: 'mfp-no-margins mfp-with-zoom', // class to remove default margin from left and right side
 		image: {
