209a9123edfe7dba038c66babaab33aed5d3460e
Yash Parekh <yparekh@uncc.edu>
2021-12-03 22:13:53 -0500

    photos

--- itis3135/project/carousel.js
+++ itis3135/project/carousel.js
@@ -1,37 +1,36 @@
-$(function(){
-  $(".carousel").ig3js({
-    manifest: [
-      {src:"DSC_0410.JPG", id:"image1"},
-      {src:"DSC_0189.JPG", id:"image2"},
-      {src:"puddle.jpg", id:"image3"},
-      {src:"DSC_0296.JPG", id:"image4"},
-      {src:"TIGERSTARE.jpg", id:"image5"},
-      {src:"DSC_0342.jpg", id:"image6"},
-      {src:"cycle2.jpg", id:"image7"},
-      {src:"leaf.jpg", id:"image8"},
-      {src:"cycle1.jpg", id:"image9"},
-    ],
-	imagePath:'images/',
-    alphaBackground:true
-  });
-});
-<script type='text/javascript'>
-			$(function(){
-				$(".box").ig3js({
-					manifest: [
-						{src:"DSC_0410.JPG", id:"image1"},
-					  {src:"DSC_0189.JPG", id:"image2"},
-					  {src:"puddle.jpg", id:"image3"},
-					  {src:"DSC_0296.JPG", id:"image4"},
-					  {src:"TIGERSTARE.jpg", id:"image5"},
-					  {src:"DSC_0342.jpg", id:"image6"},
-					  {src:"cycle2.jpg", id:"image7"},
-					  {src:"leaf.jpg", id:"image8"},
-					  {src:"cycle1.jpg", id:"image9"}
-					],
-					imagePath: 'images/',
-					alphaBackground: true
-				});
-			});
+$(document).ready(function() {
 
-			</script>
+	$('.image-popup-vertical-fit').magnificPopup({
+		type: 'image',
+		closeOnContentClick: true,
+		mainClass: 'mfp-img-mobile',
+		image: {
+			verticalFit: true
+		}
+		
+	});
+
+	$('.image-popup-fit-width').magnificPopup({
+		type: 'image',
+		closeOnContentClick: true,
+		image: {
+			verticalFit: false
+		}
+	});
+
+	$('.image-popup-no-margins').magnificPopup({
+		type: 'image',
+		closeOnContentClick: true,
+		closeBtnInside: false,
+		fixedContentPos: true,
+		mainClass: 'mfp-no-margins mfp-with-zoom', // class to remove default margin from left and right side
+		image: {
+			verticalFit: true
+		},
+		zoom: {
+			enabled: true,
+			duration: 300 // don't foget to change the duration also in CSS
+		}
+	});
+
+});
